8c0cf79a7bf1396386500e8870b180f53d5627c2
bennedictbett <benedictbett08@gmail.com>
2026-02-04 15:37:36 +0300

    strings python

---
+++ reverse_string.py
@@ -0,0 +1,31 @@
+###Reverse String Practice
+#Reverse String O(1)extra memory
+class solution:
+    def reverse_string(self, s):
+        l, r = 0, len(s) -1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l, r = l + 1, r - 1
+
+
+#Using stack space: O(n)
+class solution:
+    def reverse_string(self, s):
+        stack = []
+        for c in s:
+            stack.append(c)
+        i = 0
+        while stack:
+            s[i] = stack.pop()
+            i += 1
+
+
+ #Using Recusion Space O(n)
+class solution:
+    def ReverseString(self, s):
+
+        def reverse(l,r):
+            if l < r:
+                s[l], s[r] = s[r], s[l]
+                reverse(l + 1, r - 1)
+        reverse(0, len(s) -1)
